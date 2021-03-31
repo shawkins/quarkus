a3a81ee22a2a0b45a8bf399e9d748d69984caba9
Steven Hawkins <shawkins@redhat.com>
2021-03-30 21:39:43 -0400

    triggering error

--- core/runtime/src/main/java/io/quarkus/runtime/ApplicationConfig.java
+++ core/runtime/src/main/java/io/quarkus/runtime/ApplicationConfig.java
@@ -1,10 +1,12 @@
 package io.quarkus.runtime;
 
-import java.util.Optional;
-
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+
+
+import java.util.Optional;
+
 
 @ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public class ApplicationConfig {
